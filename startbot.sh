0a281a0f7c8ceecd3cc0b0f824796c4edf605008
incompletecoder1 <80340295+incompletecoder1@users.noreply.github.com>
2021-03-16 19:53:27 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod a+x <lichess-bot-1/engines/stockfish_13_linux_x64_modern>
+chmod a+x <engines/stockfish_13_linux_x64_modern>
 ls <linux executable directory> -l
 python lichess-bot.py
